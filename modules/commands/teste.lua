54fe1ecbd4baaf20ef37e4d9219145a864cba653
AlexandreSaito <75994293+AlexandreSaito@users.noreply.github.com>
2025-10-30 17:11:02 -0300

    u

--- modules/commands/teste.lua
+++ modules/commands/teste.lua
@@ -1,5 +1,3 @@
 function on_command(ev)
-    g.log("debug teste", ev)
-    --g.send_message(ev.Source, ev.Channel, "https://discord.gg/srWXN6KRsk")
     g.send_message(ev.Source, ev.Channel, "Comando de teste funcionando!", ev.Message.MessageId)
 end

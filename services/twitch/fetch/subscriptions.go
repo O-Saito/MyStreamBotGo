139541e1bbf8a27741fe803c8afb349bfcadfbb8
AlexandreSaito <75994293+AlexandreSaito@users.noreply.github.com>
2026-04-23 12:20:13 -0300

    fix points

--- services/twitch/fetch/subscriptions.go
+++ services/twitch/fetch/subscriptions.go
@@ -9,35 +9,39 @@ import (
 var urlAPISubscriptions = "https://api.twitch.tv/helix/subscriptions"
 
 type Subscription struct {
-	BroadcasterID            string `json:"broadcaster_id"`
-	BroadcasterLogin         string `json:"broadcaster_login"`
-	BroadcasterName         string `json:"broadcaster_name"`
-	IsGift                 bool   `json:"is_gift"`
-	GifterLogin            string `json:"gifter_login"`
-	GifterName             string `json:"gifter_name"`
-	GifterID               string `json:"gifter_id"`
-	Tier                   string `json:"tier"`
-	PlanName               string `json:"plan_name"`
-	UserID                 string `json:"user_id"`
-	UserLogin              string `json:"user_login"`
-	UserName               string `json:"user_name"`
-	SubscribedAt           string `json:"subscribed_at"`
+	BroadcasterID    string `json:"broadcaster_id"`
+	BroadcasterLogin string `json:"broadcaster_login"`
+	BroadcasterName  string `json:"broadcaster_name"`
+	IsGift           bool   `json:"is_gift"`
+	GifterLogin      string `json:"gifter_login"`
+	GifterName       string `json:"gifter_name"`
+	GifterID         string `json:"gifter_id"`
+	Tier             string `json:"tier"`
+	PlanName         string `json:"plan_name"`
+	UserID           string `json:"user_id"`
+	UserLogin        string `json:"user_login"`
+	UserName         string `json:"user_name"`
+	SubscribedAt     string `json:"subscribed_at"`
 }
 
 type GetBroadcasterSubscriptionsResponse struct {
-	Data       []Subscription `json:"data"`
-	Pagination Pagination      `json:"pagination"`
+	twitch.PaginationData[Subscription]
+	Points int `json:"points"`
 }
 
 type CheckUserSubscriptionResponse struct {
 	Data []Subscription `json:"data"`
 }
 
-func GetBroadcasterSubscriptions(userIDs []string, req *twitch.PaginationRequest) (*twitch.PaginationData[Subscription], error) {
+func GetBroadcasterSubscriptions(userIDs []string, req *twitch.PaginationRequest) (*twitch.PaginationData[Subscription], int, error) {
 	user := globals.GetState().GetTwitchUser()
 
 	opts := map[string]any{
 		"broadcaster_id": user.UserID,
+	}
+
+	if userIDs != nil {
+		opts["user_id"] = userIDs
 	}
 
 	if req != nil {
@@ -50,37 +54,40 @@ func GetBroadcasterSubscriptions(userIDs []string, req *twitch.PaginationRequest
 	}
 
 	url := twitch.BuildURL(twitch.HelixBaseURL+"/subscriptions", opts)
-	for _, id := range userIDs {
-		url += "&user_id=" + id
-	}
 
-	result, err := twitch.ExecuteRequest[twitch.PaginationData[Subscription]]("GET", url, 200)
+	result, err := twitch.ExecuteRequest[GetBroadcasterSubscriptionsResponse]("GET", url, 200)
 	if err != nil {
 		helpers.Logf(helpers.DEBUG, "[TWITCH] GetBroadcasterSubscriptions: broadcasterID=%v", user.UserID)
-		return nil, err
+		return nil, 0, err
 	}
 
-	result.GetNext = func() *twitch.PaginationData[Subscription] {
-		GetBroadcasterSubscriptions(userIDs, &twitch.PaginationRequest{
-			Cursor:   result.Pagination.Cursor,
-			Quantity: req.Quantity,
+	pagData := &twitch.PaginationData[Subscription]{
+		Data:       result.Data,
+		Pagination: result.Pagination,
+		Total:      result.Total,
+	}
+
+	pagData.GetNext = func() *twitch.PaginationData[Subscription] {
+		n, _, _ := GetBroadcasterSubscriptions(userIDs, &twitch.PaginationRequest{
+			Cursor: result.Pagination.Cursor,
 		})
-		return result
+		return n
 	}
 
-	return result, nil
+	return pagData, result.Points, nil
 }
 
-func CheckUserSubscription(broadcasterID, userID string) (*Subscription, error) {
+func CheckUserSubscription(broadcasterID string) (*Subscription, error) {
+	user := globals.GetState().GetTwitchUser()
 	opts := map[string]any{
 		"broadcaster_id": broadcasterID,
-		"user_id":       userID,
+		"user_id":        user.UserID,
 	}
 	url := twitch.BuildURL(urlAPISubscriptions, opts)
 
 	result, err := twitch.ExecuteRequest[CheckUserSubscriptionResponse]("GET", url, 200)
 	if err != nil {
-		helpers.Logf(helpers.DEBUG, "[TWITCH] CheckUserSubscription: broadcasterID=%v, userID=%v", broadcasterID, userID)
+		helpers.Logf(helpers.DEBUG, "[TWITCH] CheckUserSubscription: broadcasterID=%v, userID=%v", broadcasterID, user.UserID)
 		return nil, err
 	}
 

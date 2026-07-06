58ea54bec877e32510a5d5bdbed07a276b746e34
AlexandreSaito <75994293+AlexandreSaito@users.noreply.github.com>
2026-07-06 17:15:42 -0300

    change how to stop processor
    Cherry-picked from temporary-addons-system, removed plugin references

--- processors/chat_queue_processor.go
+++ processors/chat_queue_processor.go
@@ -9,45 +9,56 @@ import (
 
 func ProcessChatQueue() {
 	helpers.Log(helpers.INFO, "Started chat queue processor!")
-	for ev := range globals.ChatQueue {
-		config := globals.GetConfig()
-
-		// Ignore messages sent by the bot itself to prevent loops
-		if ev.MessageId == "self" {
-			globals.WsBroadcast <- globals.SocketMessage{Type: "self-message", Data: ev}
-			continue
-		}
-
-		globals.WsBroadcast <- globals.SocketMessage{Type: "user-message", Data: ev}
-
-		if strings.HasPrefix(ev.Message, config.BotPrefix) {
-			parts := strings.SplitN(ev.Message[1:], " ", 2)
-			cmd := globals.Command{
-				Source:  ev.Source,
-				Name:    parts[0],
-				Channel: ev.Channel,
-				Args:    parts[1:],
-				User:    ev.User,
-				Text:    ev.Message,
-				Message: ev,
-				Data:    map[string]any{},
+	for {
+		select {
+		case <-stopCh:
+			return
+		case ev, ok := <-globals.ChatQueue:
+			if !ok {
+				return
 			}
-			if len(parts) > 1 {
-				cmd.Args = strings.Split(parts[1], " ")
-			}
-			helpers.Printf(helpers.Yellow, "[CHAT QUEUE COMMAND] %+v", cmd)
-			globals.CommandQueue <- cmd
+			processChatMessage(ev)
 		}
-
-		mlua.DyEventQueue <- mlua.DyEventQueueData{
-			Type:              mlua.DyEventChat,
-			MessageFromStream: ev,
-		}
-		if ev.Source == "twitch" {
-			if ev.Channel != globals.GetState().TwitchUser.UserLogin {
-				continue
-			}
-		}
-		mlua.HandleChat(&ev)
 	}
+}
+
+func processChatMessage(ev globals.MessageFromStream) {
+	config := globals.GetConfig()
+
+	if ev.MessageId == "self" {
+		globals.WsBroadcast <- globals.SocketMessage{Type: "self-message", Data: ev}
+		return
+	}
+
+	globals.WsBroadcast <- globals.SocketMessage{Type: "user-message", Data: ev}
+
+	if strings.HasPrefix(ev.Message, config.BotPrefix) {
+		parts := strings.SplitN(ev.Message[1:], " ", 2)
+		cmd := globals.Command{
+			Source:  ev.Source,
+			Name:    parts[0],
+			Channel: ev.Channel,
+			Args:    parts[1:],
+			User:    ev.User,
+			Text:    ev.Message,
+			Message: ev,
+			Data:    map[string]any{},
+		}
+		if len(parts) > 1 {
+			cmd.Args = strings.Split(parts[1], " ")
+		}
+		helpers.Printf(helpers.Yellow, "[CHAT QUEUE COMMAND] %+v", cmd)
+		globals.CommandQueue <- cmd
+	}
+
+	mlua.DyEventQueue <- mlua.DyEventQueueData{
+		Type:              mlua.DyEventChat,
+		MessageFromStream: ev,
+	}
+	if ev.Source == "twitch" {
+		if ev.Channel != globals.GetState().TwitchUser.UserLogin {
+			return
+		}
+	}
+	mlua.HandleChat(&ev)
 }
